07c242a7b7248d86c7772f7492c423a79adb3687
shrekt444 <andrea.maie444@gmail.com>
2021-03-24 16:13:43 +0100

    for fatto

--- js/script.js
+++ js/script.js
@@ -5,16 +5,21 @@ var cognomeUtente = prompt('Inserisci il tuo cognome');
 console.log(cognomeUtente);
 
 mioArray.push(cognomeUtente);
-
 console.log(mioArray);
 
 mioArray.sort();
-alert(mioArray);
- 
-for ( i = 1 ; i <= mioArray.length ; i++ ) {
-    mioArray++;
+console.log(mioArray);
+document.getElementById("lista").innerHTML = mioArray;
 
-    alert(cognomeUtente);
+
+// var indiceCognome = mioArray.indexOf(cognomeUtente) + 1 ;
+// console.log('la tua posizione è:', indiceCognome );
+// alert('la tua posizione è:', indiceCognome );
+ 
+for ( i = 0 ; i < mioArray.length ; i++ ) {
+    var indiceCognome = mioArray.indexOf(cognomeUtente) + 1 ;
+    console.log('la tua posizione è:', indiceCognome );
+    document.getElementById("demo").innerHTML = indiceCognome;
 }
 
 //while ( i = 1 ; i < mioArray.length ; i++ ) {
